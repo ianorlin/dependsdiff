3ab8c707e04d6ce168488567cad1cba9e708d104
Brendan Perrine <walterorlin@gmail.com>
2014-08-25 12:24:14 -0700

    Update dependsdiff.sh
    Add in fix for issue number one checking if the package is a dependency of the metapackage which is first argument. An example is if you run ./dependsdiff.sh lubuntu-core pcmanfm

--- dependsdiff.sh
+++ dependsdiff.sh
@@ -8,11 +8,17 @@
 apt-cache depends $1 > /tmp/metapackage_dep_listing
 apt-cache depends $2 > /tmp/depends_packages
 
+#here is where should check for $2 depending on $1 in which case are done
+grep $2 /tmp/metapackage_dep_listing >/dev/null
+if [ $? -eq 0 ] ; then 
+  echo "$2 depends on on $1 and as such it should already be isntalled"
+else
+
 cat /tmp/metapackage_dep_listing >> /tmp/depends_packages 
 
 ##stop duplicate depends 
 sort --unique /tmp/depends_packages -o /tmp/depends_packages
 sort /tmp/metapackage_dep_listing -o /tmp/metapackage_dep_listing 
 
-diff /tmp/metapackage_dep_listing /tmp/depends_packages 
-
+diff /tmp/metapackage_dep_listing /tmp/depends_packages; 
+fi
